8a1bb56e696bd77b9fdb2d34358881f8d96bbfb4
AshayaRamteke98 <ashayaramteke@gmail.com>
2024-05-13 23:24:07 +0530

    que3 assignement day8

---
+++ Day8_Assignements/que3.c
@@ -0,0 +1,42 @@
+//3. Find max number of child processes that can be created on Linux using C program? 
+//Hint: use fork() in inﬁnite loop (wisely).
+
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
+
+int main(){
+	
+	int ret,s,i;
+	i=1;
+	while(1){
+
+		ret = fork();
+		if(ret == 0){
+
+			printf("inside child task \n");
+			_exit(0);
+		}
+
+		else if(ret == -1){
+
+			printf("fork() failed");
+			break;
+		}
+
+		else {
+
+			i++;
+			printf("child count : %d\n",i);
+		}
+
+	}
+
+	for(int j=1;j<=i;j++){
+		waitpid(-1,&s,0);
+		printf("WEXITSTATUS = %d\n",WEXITSTATUS(s));
+		}
+
+	return 0;
+}
+
